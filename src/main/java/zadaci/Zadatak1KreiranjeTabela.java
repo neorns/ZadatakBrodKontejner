777247de37ff812f6cfd4bcaad15173e7babb189
Nenad <norlovic@gmail.com>
2017-01-20 18:54:06 +0100

    Uradjen zadatak kreiranja tabela. Closes #5

--- src/main/java/zadaci/Zadatak1KreiranjeTabela.java
+++ src/main/java/zadaci/Zadatak1KreiranjeTabela.java
@@ -2,6 +2,9 @@ package zadaci;
 
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+import model.Brod;
+import model.Kontejner;
 
 import java.io.IOException;
 
@@ -14,6 +17,12 @@ public class Zadatak1KreiranjeTabela {
         try {
             // create our data-source for the database
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+            TableUtils.dropTable(connectionSource, Kontejner.class,true);
+            TableUtils.dropTable(connectionSource, Brod.class,true);
+
+            TableUtils.createTable(connectionSource,Brod.class);
+            TableUtils.createTable(connectionSource,Kontejner.class);
+
 
         }catch (Exception e) {
             e.printStackTrace();
