a96604593b5acf38357fd123d724abed18b529d8
Nenad <norlovic@gmail.com>
2017-01-20 19:16:02 +0100

    Uradjen zadatak izmena vrednosti. Closes #7

--- src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
+++ src/main/java/zadaci/Zadatak3IzmenaVrednosti.java
@@ -1,9 +1,15 @@
 package zadaci;
 
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import model.Kontejner;
 
 import java.io.IOException;
+import java.util.List;
+
+import static model.Kontejner.POLJE_OPIS;
+import static zadaci.Zadatak2DodavanjeVrednosti.kontejnerDao;
 
 /**
  * Created by androiddevelopment on 20.1.17..
@@ -14,6 +20,21 @@ public class Zadatak3IzmenaVrednosti {
         try {
             // create our data-source for the database
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:brodKontejner.db");
+
+            kontejnerDao = DaoManager.createDao(connectionSource, Kontejner.class);
+            List<Kontejner> kontejneri = kontejnerDao.queryForAll();
+            for (Kontejner k : kontejneri)
+                System.out.println(k);
+
+            kontejneri=kontejnerDao.queryForEq(POLJE_OPIS,"Morski proizvodi");
+            Kontejner kontejnerZaIzmenu = kontejneri.get(0);
+            kontejnerZaIzmenu.setOpis("Plodovi mora");
+            kontejnerDao.update(kontejnerZaIzmenu);
+
+            kontejneri = kontejnerDao.queryForAll();
+            for (Kontejner k : kontejneri)
+                System.out.println(k);
+
 
         }catch (Exception e) {
             e.printStackTrace();
